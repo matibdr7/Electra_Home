8ad454bb8c17368af591eac343b99c6779f1c080
@matibdr7 <matias.bdr7@gmail.com>
2024-11-08 01:23:34 -0300

    Implemento funcionalidad de contacto con mail

--- team_ALMP/core/views.py
+++ team_ALMP/core/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import HttpResponse,render
+from django.core.mail import send_mail
+from django.conf import settings
 
 def index(request):
     return render(request, 'core/index.html')
@@ -9,5 +11,16 @@ def acercade(request):
     return render(request, 'core/acercade.html')
 
 def contacto(request):
+    if request.method == "POST":
+        subject=request.POST["subject"]
+        
+        message=request.POST["message"] + "" + request.POST["email"]
+        
+        email_from=settings.EMAIL_HOST_USER
+        
+        recipient_list=["electrahome99@gmail.com"]
+        
+        send_mail(subject, message, email_from, recipient_list)
+    
     return render(request, 'core/contacto.html')
 
