4db035b3bae0b8a09373f8a47de3f2edda4c92a5
geronimojohn94 <geronimojohn94@gmail.com>
2024-11-09 17:30:19 -0300

    Correcion de errores en models de la app

--- team_ALMP/ventas/models.py
+++ team_ALMP/ventas/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # Venta: fecha, Producto, Cliente, precio, created, updated.
 # Create your models here.
-<<<<<<< HEAD
 class Venta(models.Model):
     fecha: str = models.CharField(max_length=10, verbose_name="Fecha")
     producto: str = models.CharField(max_length=200, verbose_name="Producto")
@@ -37,7 +36,6 @@ class Cliente(models.Model):
     def __str__(self):
         return self.nombre
     
-=======
 class Producto (models.Model):
     codigo = models.CharField(max_length=20, unique=True, verbose_name= "Código")
     nombre = models.CharField(max_length=100,verbose_name="Nombre")
@@ -73,6 +71,3 @@ class Proveedor(models.Model):
 
     def __str__(self):
         return self.nombre
-
->>>>>>> Featured-Producto
-    
