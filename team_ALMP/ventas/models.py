ceb9da5c34f1e766240b7ecb45bc0850f019f99f
@matibdr7 <matias.bdr7@gmail.com>
2024-11-09 17:22:30 -0300

    COrrecion models.py

--- team_ALMP/ventas/models.py
+++ team_ALMP/ventas/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # Venta: fecha, Producto, Cliente, precio, created, updated.
 # Create your models here.
-<<<<<<< HEAD
 class Venta(models.Model):
     fecha: str = models.CharField(max_length=10, verbose_name="Fecha")
     producto: str = models.CharField(max_length=200, verbose_name="Producto")
@@ -18,7 +17,6 @@ class Venta(models.Model):
     
     def __str__(self):
         return self.fecha
-=======
 
 #Cliente: nombre, apellido, email, celular, foto, created, updated.
 class Cliente(models.Model):
@@ -38,5 +36,4 @@ class Cliente(models.Model):
     def __str__(self):
         return self.nombre
     
->>>>>>> Feature-Cliente
     
