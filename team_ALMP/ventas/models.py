8c63456340c56b7356dd8b02b91feffe4738b0da
@matibdr7 <matias.bdr7@gmail.com>
2024-11-09 17:11:28 -0300

    Modelo cliente creado

--- team_ALMP/ventas/models.py
+++ team_ALMP/ventas/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+#Cliente: nombre, apellido, email, celular, foto, created, updated.
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=50, verbose_name="Nombre")
+    apellido = models.CharField(max_lenght=50, verbose_name="Apellido")
+    email = models.EmailField(verbose_name="Email")
+    celular = models.IntegerField(verbose_name="Celular")
+    foto = models.ImageField(verbose_name="Foto")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now = True)
+
+    class Meta:
+        verbose_name = "cliente"
+        verbose_name_plural = "clientes"
+        ordering = ["-created"] #Orden descendente
+        
+    def __str__(self):
+        return self.nombre
+    
+    
