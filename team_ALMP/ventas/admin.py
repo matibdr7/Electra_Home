eeae57b8072ccff7fe8c6f4ffe8a64b95b739a6d
geronimojohn94 <geronimojohn94@gmail.com>
2024-11-09 17:51:19 -0300

    habilitación de modelo en panel administrador

--- team_ALMP/ventas/admin.py
+++ team_ALMP/ventas/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Cliente,Venta,Producto,Proveedor
 # Register your models here.
+admin.site.register(Cliente)
+admin.site.register(Venta)
+admin.site.register(Producto)
+admin.site.register(Proveedor)
