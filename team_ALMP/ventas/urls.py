1c8ab76343f95102ed9d3b78f294ff65737e0a17
@matibdr7 <matias.bdr7@gmail.com>
2024-11-12 23:42:40 -0300

    Correccion de error

--- team_ALMP/ventas/urls.py
+++ team_ALMP/ventas/urls.py
@@ -4,6 +4,5 @@ from django.urls import path, include
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('modelos/', views.modelos, name="modelos")
 ]
