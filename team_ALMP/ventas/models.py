31b49fa03afff52675f0ece8ee3caa2931f11dfe
@matibdr7 <matias.bdr7@gmail.com>
2024-11-15 00:32:55 -0300

    Relaciones entre modelos aplicados

--- team_ALMP/ventas/models.py
+++ team_ALMP/ventas/models.py
@@ -2,21 +2,10 @@ from django.db import models
 
 # Venta: fecha, Producto, Cliente, precio, created, updated.
 # Create your models here.
-class Venta(models.Model):
-    fecha = models.DateTimeField(verbose_name="Fecha")
-    producto = models.CharField(max_length=200, verbose_name="Producto")
-    cliente = models.CharField(max_length=50, verbose_name="Cliente")
-    precio = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Precio")
-    created = models.DateTimeField(auto_now_add=True, verbose_name="Crear")
-    updated = models.DateTimeField(auto_now=True, verbose_name="Actualizar")
 
-    class Meta:
-        verbose_name = "Venta"
-        verbose_name_plural = "Ventas"
-        ordering = ["-created"]
-    
-    def __str__(self):
-        return f"Venta del {self.fecha.strftime('%d-%m-%Y %H:%M')} - Cliente: {self.cliente}"
+#Cliente y Venta: Relación uno a muchos, cada venta tiene un solo cliente, pero un cliente puede tener muchas ventas.
+#Producto y Venta: Uno a uno porque una venta puede estar relacionado a un producto y un producto a una venta
+#Proveedor y Producto: Uno a muchos porque un proveedor puede proveer muchos productos, pero un producto puede tener un solo proveedor
 
 #Cliente: nombre, apellido, email, celular, foto, created, updated.
 class Cliente(models.Model):
@@ -35,27 +24,6 @@ class Cliente(models.Model):
         
     def __str__(self):
         return self.nombre
-    
-class Producto (models.Model):
-    codigo = models.CharField(max_length=20, unique=True, verbose_name= "Código")
-    nombre = models.CharField(max_length=100,verbose_name="Nombre")
-    descripcion = models.TextField(verbose_name="Descripción")
-    precio = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Precio")
-    cantidad=models.PositiveIntegerField(default=0, verbose_name="Cantidad")
-    imagen=models.ImageField(verbose_name="Imágen", upload_to="productos")
-    imagenExtra1=models.ImageField(verbose_name="ImágenExtra1", upload_to="productos", null=True, blank=True)
-    imagenExtra2=models.ImageField(verbose_name="ImágenExtra2", upload_to="productos", null=True, blank=True)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        verbose_name ="producto"
-        verbose_name_plural = "productos"
-        ordering = ["-created"]
-    
-    def __str__(self):
-        return f'{self.nombre} ({self.codigo})'
-
 
 class Proveedor(models.Model):
     nombre = models.CharField(max_length=50, verbose_name="Nombre")
@@ -73,3 +41,40 @@ class Proveedor(models.Model):
 
     def __str__(self):
         return self.nombre
+
+class Producto (models.Model):
+    codigo = models.CharField(max_length=20, unique=True, verbose_name= "Código")
+    nombre = models.CharField(max_length=100,verbose_name="Nombre")
+    descripcion = models.TextField(verbose_name="Descripción")
+    precio = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Precio")
+    cantidad=models.PositiveIntegerField(default=0, verbose_name="Cantidad")
+    imagen=models.ImageField(verbose_name="Imágen", upload_to="productos")
+    imagenExtra1=models.ImageField(verbose_name="ImágenExtra1", upload_to="productos", null=True, blank=True)
+    imagenExtra2=models.ImageField(verbose_name="ImágenExtra2", upload_to="productos", null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    proveedor = models.ForeignKey(Proveedor, on_delete=models.CASCADE, verbose_name="Proveedor", null=True, blank=True)
+
+    class Meta:
+        verbose_name ="producto"
+        verbose_name_plural = "productos"
+        ordering = ["-created"]
+    
+    def __str__(self):
+        return f'{self.nombre} ({self.codigo})'
+
+class Venta(models.Model):
+    fecha = models.DateTimeField(verbose_name="Fecha")
+    precio = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Precio")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Crear")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Actualizar")
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, verbose_name="Cliente", null=True, blank=True)  # Relación uno a muchos
+    producto = models.OneToOneField(Producto, on_delete=models.CASCADE, verbose_name="Producto", null=True, blank=True)  # Relación uno a uno
+
+    class Meta:
+        verbose_name = "Venta"
+        verbose_name_plural = "Ventas"
+        ordering = ["-created"]
+    
+    def __str__(self):
+        return f"Venta del {self.fecha.strftime('%d-%m-%Y %H:%M')} - Cliente: {self.cliente}"
