8333f5f5473555d4e153e968b4b0e0d7530fe1be
@matibdr7 <matias.bdr7@gmail.com>
2024-11-20 00:16:40 -0300

    Funcionando actualizar y agregar cliente

--- team_ALMP/ventas/views.py
+++ team_ALMP/ventas/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from .models import Venta, Cliente, Producto, Proveedor
 from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.list import ListView
+from django.utils import timezone
+
 from django.urls import reverse_lazy
 
 def modelos(request):
@@ -29,5 +32,12 @@ class ClienteUpdate(UpdateView):
     template_name_suffix = '_update_cliente'
     
     def get_success_url(self):
-        return reverse_lazy('modelos')+'?Actualizado'
+        return reverse_lazy('modelos')
+
+class ClienteList(ListView):
+    model = Cliente
     
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(self, **kwargs)
+        context['now'] = timezone.now()
+        return context
